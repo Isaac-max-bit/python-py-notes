0c91dadef62f2a3a43d6fc2bfce7791dc3fff9f8
Isaac Alejandro García amaya <isaac-garcia@192.168.40.107>
2026-07-03 22:10:57 -0500

    finish class

---
+++ main.py
@@ -0,0 +1,14 @@
+# print("I love Resident evil saga")
+# print("Really?, so do I!")
+
+# Variable = A container for a value (String, iteger, float, boolean)
+
+# A variable is a reserved memory location to store values. In other words, a variable in a python program gives data to the computer for processing.
+
+first_name = "Isaac"
+food = "spaghetti with tuna"
+last_name = "García"
+
+print(f"Hello, {first_name}!")
+print(f"My favorite food is {food}.")
+print(f"{first_name} {last_name}")
